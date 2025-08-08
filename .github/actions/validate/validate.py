9b15104efa3802199e3ef104409b767b940414bd
Abhay Mishra <grabhaymishra@gmail.com>
2025-08-08 15:12:50 +0530

    updated code

--- .github/actions/validate/validate.py
+++ .github/actions/validate/validate.py
@@ -14,31 +14,33 @@ REPO_WORKSPACE = Path("/github/workspace")
 
 def main():
     """Automatically finds and validates an OWASP metadata file."""
-    sys.stdout.write("INFO: Searching for OWASP metadata file.")
+    sys.stdout.write("INFO: Searching for OWASP metadata file.\n")
 
     found_files = list(REPO_WORKSPACE.glob("*.owasp.yaml"))
 
     if not found_files:
-        sys.stderr.write("ERROR: No OWASP metadata file found.")
+        sys.stderr.write("ERROR: No OWASP metadata file found.\n")
         sys.exit(1)
 
     if len(found_files) > 1:
-        sys.stderr.write("ERROR: Multiple OWASP metadata files found.")
+        sys.stderr.write("ERROR: Multiple OWASP metadata files found.\n")
         sys.exit(1)
 
     file_path = found_files[0]
     file_name = file_path.name
     schema_name = file_name.split(".")[0]
 
-    sys.stdout.write(f"INFO: Found '{file_name}'. Validating against the '{schema_name}' schema.")
+    sys.stdout.write(
+        f"INFO: Found '{file_name}'. Validating against the '{schema_name}' schema.\n",
+    )
 
     schema = get_schema(schema_name)
     with file_path.open("r") as f:
         data = yaml.safe_load(f)
 
     if error_message := validate_data(schema=schema, data=data):
-        sys.stderr.write(f"ERROR: Validation failed! {error_message}")
+        sys.stderr.write(f"ERROR: Validation failed! {error_message}\n")
         sys.exit(1)
     else:
-        sys.stdout.write("SUCCESS: Validation successful.")
+        sys.stdout.write("SUCCESS: Validation successful.\n")
         sys.exit(0)
